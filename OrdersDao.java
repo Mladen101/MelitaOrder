961c02725c7fbcfc3b40a85170b14c6f4707fda4
Mladen101 <111059374+Mladen101@users.noreply.github.com>
2022-12-13 00:09:13 +0100

    Add files via upload

---
+++ OrdersDao.java
@@ -0,0 +1,35 @@
+package com.proj.model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author HP
+ 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author admin
+ */
+@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+public class OrdersDao {
+    @Autowired
+    SessionFactory sessionFactory;
+    
+    public void save(Orders order){
+       Session session = sessionFactory.getCurrentSession();
+       //session.beginTransaction(); 
+       session.save(order); 
+       //session.getTransaction().commit();
+    }
+}
