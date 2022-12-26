96da4c905799109aa28b3e9484efb6b7903acc21
Mladen101 <111059374+Mladen101@users.noreply.github.com>
2022-12-26 08:44:08 +0100

    Add files via upload

---
+++ KafkaProducerApplication.java
@@ -0,0 +1,19 @@
+package com.springkafka.kafkaproducer;
+
+
+
+	import com.springkafka.kafkaproducer.config.KafkaConfig;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.cloud.stream.annotation.EnableBinding;
+
+	@SpringBootApplication
+	@EnableBinding(KafkaConfig.class)
+	public class KafkaProducerApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(KafkaProducerApplication.class, args);
+		}
+
+	}
+
