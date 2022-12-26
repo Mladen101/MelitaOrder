28f1b3621531e13d667061260f4f108e10f5980f
Mladen101 <111059374+Mladen101@users.noreply.github.com>
2022-12-26 08:39:42 +0100

    Add files via upload

---
+++ Producer.java
@@ -0,0 +1,15 @@
+package com.kafka.producer.src;
+
+
+	//package com.springkafka.kafkaproducer.config;
+
+	import org.springframework.cloud.stream.annotation.Output;
+	import org.springframework.messaging.MessageChannel;
+
+	public interface KafkaConfig {
+		@Output
+	    
+	    public MessageChannel kafkaExample();
+
+	}
+
