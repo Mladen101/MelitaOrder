3b8f60a3862f357ba5a14cd88961d3a6223a5a35
Mladen101 <111059374+Mladen101@users.noreply.github.com>
2022-12-26 08:47:20 +0100

    Add files via upload

---
+++ SimpleGrantedAuthority.java
@@ -0,0 +1,9 @@
+package com.springboot.service_layer;
+
+public class SimpleGrantedAuthority {
+	
+	 void role(){}
+	Object getName(){
+  return getName();}
+
+}
--- UserRepository.java
+++ UserRepository.java
@@ -1,17 +1,10 @@
-package com.bezkoder.springjwt.repository;
+package com.springboot.service_layer;
 
-import java.util.Optional;
+public class UserRepository {
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+	public Object findByUsernameOrEmail(String usernameOrEmail, String usernameOrEmail2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-import com.bezkoder.springjwt.models.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUsername(String username);
-
-  Boolean existsByUsername(String username);
-
-  Boolean existsByEmail(String email);
 }
