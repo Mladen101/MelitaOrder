9b12ed2262d37685b5ed521fc6ee9599d0414f6c
Mladen101 <111059374+Mladen101@users.noreply.github.com>
2023-01-02 22:05:53 +0100

    Add files via upload

---
+++ SpringApplication.java
@@ -0,0 +1,21 @@
+package org.springframework.boot;
+
+import com.springboot.app.Application;
+import com.springkafka.kafkaproducer.KafkaProducerApplication;
+
+public class SpringApplication {
+
+	public static void run(Class<Application> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	
+
+
+	
+
+
+}
